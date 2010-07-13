2db919aeabd4c17efc1ccb6e7401f55fdfe99192
emijrp <emijrp@gmail.com>
2010-07-13 21:57:21 +0000

    git-svn-id: https://toolserver.googlecode.com/svn/trunk/wikimania@398 c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <center>
 <table width="800px">
 	<tr>
-		<td rowspan=2 valign=top><iframe src="channel1.php" width="400" height="400"></iframe><br/><center>Also, <a href="http://commons.wikimedia.org/wiki/Category:Wikimania_2010" target="_blank">photos</a> in Wikimedia Commons<br/><br/><small>Powered by: <a href="http://wikimania2010.wikimedia.org" target="_blank">Wikimania</a> and <a href="http://toolserver.org" target="_blank">Toolserver</a><br/>Credits: background image by <a href="http://pl.wikipedia.org/wiki/Wikipedysta:Airwolf">Airwolf</a> (CC-by-sa)</small></center></td>
+		<td rowspan=2 valign=top><iframe src="channel1.php" width="400" height="400"></iframe><br/><center>Also, <a href="http://commons.wikimedia.org/wiki/Category:Wikimania_2010" target="_blank">photos</a> in Wikimedia Commons<br/><br/><small>Thanks to <a href="http://www.istream.pl/">iStream</a>, <a href="http://wikimania2010.wikimedia.org" target="_blank">Wikimania</a> and <a href="http://toolserver.org" target="_blank">Toolserver</a><br/>Credits: background image by <a href="http://pl.wikipedia.org/wiki/Wikipedysta:Airwolf">Airwolf</a> (CC-by-sa)</small></center></td>
 		<td valign=top><iframe src="http://webchat.freenode.net?channels=wikimania" width="575" height="300"></iframe></td>
 	</tr>
 	<tr>
