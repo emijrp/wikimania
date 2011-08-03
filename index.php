e5404f70871316f6414666b7195d69d32b928c6b
emijrp <emijrp@gmail.com>
2011-08-03 09:04:26 +0000

    wikimania 2011;
    git-svn-id: https://toolserver.googlecode.com/svn/trunk/wikimania@650 c6dfa21e-ad1b-11dd-a35d-6bd55b4a219c

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Wikimania 2010</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<title>Wikimania 2011</title>
 <script type="text/javascript" src="jquery.min.js"></script>
 <script type="text/javascript" src="monitter.min.js"></script>
 <link rel="stylesheet" href="monitter.css" />
 </head>
-<body style="background-image: url('background.jpg');">
-<h1>Wikimania 2010 (<a href="http://wikimania2010.wikimedia.org" target="_blank">http://wikimania2010.wikimedia.org</a>)</h1>
+<body style="background-image: url('background.jpg');background-repeat: no-repeat;background-position: center top;">
+<span style="text-align: center;position: absolute; top: 10px; left: 10px;">
+[<a href="http://toolserver.org/~emijrp/" target="_blank">More emijrp tools</a>]
+</span>
+<span style="text-align: center;position: absolute; top: 10px; right: 10px;">
+[<a href="http://commons.wikimedia.org/wiki/Commons:Upload" target="_blank"><b>Upload</b></a> your <a href="http://commons.wikimedia.org/wiki/Category:Wikimania_2011" target="_blank">Wikimania photos</a>]
+</span>
+<center><big><big><big><b><a href="http://wikimania2011.wikimedia.org" target="_blank">Wikimania <i>2011</i></a></b></big></big></big><br/><b>Haifa, Israel, August 4–7, 2011</b><br/><br/>Previous Wikimanias: <a href="http://wikimania2010.wikimedia.org" target="_blank">Poland</a> (2010), <a href="http://wikimania2009.wikimedia.org" target="_blank">Argentine</a> (2009), <a href="http://wikimania2008.wikimedia.org" target="_blank">Egypt</a> (2008), <a href="http://wikimania2007.wikimedia.org" target="_blank">Taiwan</a> (2007), <a href="http://wikimania2006.wikimedia.org" target="_blank">United States</a>(2006), <a href="http://wikimania2005.wikimedia.org" target="_blank">Germany</a> (2005)</center>
 <center>
-<table width="800px">
+<table>
 	<tr>
-		<td rowspan=2 valign=top><iframe src="channel1.php" width="400" height="400"></iframe><br/><center>Also, <a href="http://commons.wikimedia.org/wiki/Category:Wikimania_2010" target="_blank">photos</a> in Wikimedia Commons<br/><br/><small>Thanks to <a href="http://www.istream.pl/">iStream</a>, <a href="http://wikimania2010.wikimedia.org" target="_blank">Wikimania</a> and <a href="http://toolserver.org" target="_blank">Toolserver</a><br/>Credits: background image by <a href="http://pl.wikipedia.org/wiki/Wikipedysta:Airwolf">Airwolf</a> (CC-by-sa)</small></center></td>
-		<td valign=top><iframe src="http://webchat.freenode.net?channels=wikimania" width="575" height="300"></iframe></td>
+		<td rowspan=2 valign=top>
+        <div class="monitter" id="tweets" title="wikimania"></div>
+        <center>Include the word "Wikimania" in your tweet<br/>and it will be showed here</center>
+        </td>
+		<td valign=top>
+        <iframe width="575" height="290" src="http://www.youtube-nocookie.com/embed/H4MAxefJfBY?rel=0" frameborder="0" allowfullscreen></iframe>
+        </td>
 	</tr>
 	<tr>
-		<td valign=top><div class="monitter" id="tweets" title="wikimania"></div></td>
+		<td valign=top><iframe src="http://webchat.freenode.net?channels=wikimania" width="575" height="250"></iframe></td>
 	</tr>
 </table>
-More <a href="http://toolserver.org/~emijrp/">emijrp</a> tools
+<small>Thanks to <a href="http://wikimania2011.wikimedia.org" target="_blank">Wikimania</a> and <a href="http://toolserver.org" target="_blank">Toolserver</a>. <a href="http://wikimania2011.wikimedia.org/wiki/File:Haifa_wikimania_3.png" target="_blank">Background image details</a></small>
 </center>
 </body>
 </html>
